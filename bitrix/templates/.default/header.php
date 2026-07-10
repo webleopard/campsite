0da11191025fd1e6b8da2b597a745f081b4ee8a1
Dmitry Ivanov <dmitryiva@gmail.com>
2026-07-10 14:29:14 +0400

    bitrix update

--- bitrix/templates/.default/header.php
+++ bitrix/templates/.default/header.php
@@ -1,4 +1,4 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+<?php if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 IncludeTemplateLangFile(__FILE__);
 
 /**
@@ -8,22 +8,22 @@ IncludeTemplateLangFile(__FILE__);
 ?>
 <html>
 <head>
-<?$APPLICATION->ShowHead();?>
-<title><?$APPLICATION->ShowTitle()?></title>
+<?php $APPLICATION->ShowHead();?>
+<title><?php $APPLICATION->ShowTitle()?></title>
 </head>
 
 <body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" bgcolor="#FFFFFF">
 
-<?$APPLICATION->ShowPanel()?>
+<?php $APPLICATION->ShowPanel()?>
 
-<?if($USER->IsAdmin()):?>
+<?php if($USER->IsAdmin()):?>
 
 <div style="border:red solid 1px">
 	<form action="/bitrix/admin/site_edit.php" method="GET">
 		<input type="hidden" name="LID" value="<?=SITE_ID?>" />
-		<p><font style="color:red"><?echo GetMessage("DEF_TEMPLATE_NF")?> </font></p>
-		<input type="submit" name="set_template" value="<?echo GetMessage("DEF_TEMPLATE_NF_SET")?>" />
+		<p><font style="color:red"><?= GetMessage("DEF_TEMPLATE_NF")?> </font></p>
+		<input type="submit" name="set_template" value="<?= GetMessage("DEF_TEMPLATE_NF_SET")?>" />
 	</form>
 </div>
 
-<?endif?>
+<?php endif?>
