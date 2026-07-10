181b6f263ecb50fd6d6a2d1108b08377afb926bd
Dmitry Ivanov <dmitryiva@gmail.com>
2026-07-10 14:50:27 +0400

    Все формы обр.связи - чекбокс без подписи сейчас почему-то

--- local/php_interface/user_lang/ru/lang.php
+++ local/php_interface/user_lang/ru/lang.php
@@ -1,3 +1,6 @@
 <?php
 // $MESS["/local/templates/aspro-allcorp3_dev_s2/components/bitrix/news/partners/lang/ru/detail.php"]['BACK_LINK'] = 'Показать всех';
+$MESS["/bitrix/templates/bs_hotel_s1/components/brainsite/iblock.element.add.form/main_form/lang/ru/template.php"]['FORM_AGREEMENT'] = 'Соглашаюсь с <a href="/aditional/license/" target="_blank">политикой обработки персональных данных</a>';
+$MESS["/bitrix/templates/bs_hotel_s1/components/brainsite/iblock.element.add.form/popup_form/lang/ru/template.php"]['FORM_AGREEMENT'] = 'Соглашаюсь с <a href="/aditional/license/" target="_blank">политикой обработки персональных данных</a>';
+$MESS["/bitrix/templates/bs_hotel_s1/components/brainsite/iblock.element.add.form/popup_form_hotel/lang/ru/template.php"]['FORM_AGREEMENT'] = 'Соглашаюсь с <a href="/aditional/license/" target="_blank">политикой обработки персональных данных</a>';
 ?>
