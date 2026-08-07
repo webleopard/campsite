5b3166d9386f463b3b9d9a6e2c95dbb3c8946884
Dmitry Ivanov <dmitryiva@gmail.com>
2026-08-07 14:45:30 +0400

    Слово Вас с маленькой буквы в формах

--- local/php_interface/user_lang/ru/lang.php
+++ local/php_interface/user_lang/ru/lang.php
@@ -3,4 +3,7 @@
 $MESS["/bitrix/templates/bs_hotel_s1/components/brainsite/iblock.element.add.form/main_form/lang/ru/template.php"]['FORM_AGREEMENT'] = 'Соглашаюсь с <a href="/aditional/license/" target="_blank">политикой обработки персональных данных</a>';
 $MESS["/bitrix/templates/bs_hotel_s1/components/brainsite/iblock.element.add.form/popup_form/lang/ru/template.php"]['FORM_AGREEMENT'] = 'Соглашаюсь с <a href="/aditional/license/" target="_blank">политикой обработки персональных данных</a>';
 $MESS["/bitrix/templates/bs_hotel_s1/components/brainsite/iblock.element.add.form/popup_form_hotel/lang/ru/template.php"]['FORM_AGREEMENT'] = 'Соглашаюсь с <a href="/aditional/license/" target="_blank">политикой обработки персональных данных</a>';
+$MESS["/bitrix/templates/bs_hotel_s1/components/brainsite/iblock.element.add.form/main_form/lang/ru/template.php"]['FORM_COMMENT'] = 'Отправьте заявку и мы проконсультируем вас по всем вопросам!';
+$MESS["/bitrix/templates/bs_hotel_s1/components/brainsite/iblock.element.add.form/popup_form/lang/ru/template.php"]['FORM_COMMENT'] = 'Отправьте заявку и мы проконсультируем вас по всем вопросам!';
+$MESS["/bitrix/templates/bs_hotel_s1/components/brainsite/iblock.element.add.form/popup_form_hotel/lang/ru/template.php"]['FORM_COMMENT'] = 'Отправьте заявку и мы проконсультируем вас по всем вопросам!';
 ?>
