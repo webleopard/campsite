cc827323fcd271262519a0a1313a8cfd68a852e4
Dmitry Ivanov <dmitryiva@gmail.com>
2026-08-18 15:22:09 +0400

    Заголовок на карте

--- bitrix/templates/bs_hotel_s1/include/blocks/map.php
+++ bitrix/templates/bs_hotel_s1/include/blocks/map.php
@@ -8,7 +8,7 @@
 		"AREA_FILE_RECURSIVE" => "Y",
 		"EDIT_TEMPLATE" => ".default",
 		"COORDS" => "53.415924, 50.124587",
-		"FORM_HEADER" => "Адрес базы отдыха САЛЮТ"
+		"FORM_HEADER" => "Адрес базы отдыха «Салют»"
 	],
 	false
 );
